ecce123b699a5556b823d8ed3fdccbb01cd57a3d
Shubham-Lal <im.shubhamlal@gmail.com>
2024-09-04 22:20:29 +0530

    server: fix swagger issue

--- server/server.js
+++ server/server.js
@@ -36,14 +36,12 @@ const options = {
     apis: ['./routes/*.js']
 }
 const specs = swaggerJsdoc(options)
-const CSS_URL = 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css'
 app.use(
     '/',
     swaggerUi.serve,
     swaggerUi.setup(specs, {
         customSiteTitle: 'Certificate Site API',
-        customfavIcon: 'favicon.ico',
-        customCssUrl: CSS_URL
+        customfavIcon: 'favicon.ico'
     })
 )
 
