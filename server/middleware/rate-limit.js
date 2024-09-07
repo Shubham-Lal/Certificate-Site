9cddf89a6edcefe14f9695989f3734e07502de72
Shubham Lal <im.shubhamlal@gmail.com>
2024-09-07 18:07:26 +0530

    server: update rate limit

--- server/middleware/rate-limit.js
+++ server/middleware/rate-limit.js
@@ -2,10 +2,10 @@ const rateLimit = require('express-rate-limit')
 
 const rateLimiter = (action) => rateLimit({
     windowMs: 60 * 60 * 1000,
-    max: 1,
+    max: 2,
     message: {
         success: false,
-        message: `Exceeded Limit: You can only ${action} certificate once per hour!`,
+        message: `Exceeded Limit: You can only ${action} certificate twice per hour!`,
     },
     keyGenerator: (req) => `${req.admin._id.toString()}:${action}`,
     skipFailedRequests: true
